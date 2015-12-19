59c76d60de332d32d45981ece21dec1728ed99e2
Jiongye Li <jiongye_li@hms.harvard.edu>
2015-12-18 22:50:12 -0500

    worked on week #3 home work

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -22,9 +22,8 @@ grad = zeros(size(theta));
 
 J = (1 / m) * sum(-y' * log(sigmoid(X * theta)) - (1 - y)' * log(1 - sigmoid(X * theta)));
 
-grad = (1/m) * sum(X' * (sigmoid(X * theta) - y));
+grad = (1/m) * sum(X .* (sigmoid(X * theta) - y));
 
-%grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
 
 % =============================================================
 
