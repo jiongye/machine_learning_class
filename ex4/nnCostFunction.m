ecc157cd8ee03bc490d246a823a83b8418aa66d1
Jiongye Li <jiongye@gmail.com>
2015-12-30 15:01:38 -0500

    finished week #5 home work

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 %
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,22 +62,34 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+for i = 1:m
+  x = X(i, :);
+  yvec = 1:num_labels == y(i);
+
+  z2 = x * Theta1';
+  a2 = sigmoid(z2);
+  a2 = [1 a2];
+
+  z3 = a2 * Theta2';
+  a3 = sigmoid(z3);
+
+  J = J + (1/m) * sum(-yvec .* log(a3) - (1 - yvec) .* log(1 - a3));
+
+  delta3 = a3 - yvec;
+  delta2 = Theta2' * delta3' .* sigmoidGradient([1 z2])';
+
+  Theta1_grad = Theta1_grad .+ (1/m) * (delta2(2:end) .* x);
+  Theta2_grad = Theta2_grad .+ (1/m) * (delta3' * a2);
+
+end
+
+J = J + lambda/(2*m)*(sum(Theta1(:, 2:end)(:).^2) + sum(Theta2(:, 2:end)(:).^2));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) .+ lambda/m * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) .+ lambda/m * Theta2(:, 2:end);
 
 
 % -------------------------------------------------------------
